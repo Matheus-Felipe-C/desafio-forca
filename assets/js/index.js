074206f504b43f51e5b8709ee04c6585a5621ad3
Matheus Felipe de Oliveira Ferreira <theusf40@hotmail.com>
2022-09-09 18:03:16 +0000

    Adicionado html da página de adição

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,11 @@
 elem.addEventListener("click", () => {
     count = teste(count)
 })*/
+
+function irAdd() {
+    location.href = '../../addPalavra.html'
+}
+
+function irHome() {
+    location.href = '../../index.html'
+}
