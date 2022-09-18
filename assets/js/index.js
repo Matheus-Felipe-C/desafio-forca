9baa87fe185fef904aa2454b832b6b6920fadd16
Matheus Felipe de Oliveira Ferreira <theusf40@hotmail.com>
2022-09-18 00:20:04 +0000

    Adicionado desenho dos traços baseado no tamanho da palavra

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,27 @@
 const btn = document.getElementById('btn')
+const canvas = document.getElementById('jogo')
 btn.addEventListener("click", escolherPalavra)
 
-let palavras = ["jogo", "teste", 'banana']
+let palavras = ['papagaio', 'teste', 'banana', 'batata']
 
 function escolherPalavra() {
     let palavraSecreta = palavras[Math.floor(Math.random() * palavras.length)]
     console.log(palavraSecreta)
+    desenharPalavra(palavraSecreta)
+}
+
+function desenharPalavra(palavra) {
+    if (canvas.getContext) {
+        const caneta = canvas.getContext('2d')
+        let x = 25;
+
+        for(let i = 0; i < palavra.length; i++) {
+            caneta.fillStyle = '#0A3871'
+            caneta.fillRect(x, 125, 25, 2)
+
+            x += 35
+        }
+    }
 }
 
 function irAdd() {
