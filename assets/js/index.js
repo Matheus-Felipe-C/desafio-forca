6eaa7ef59a0470e854edd8d8270b58644bb25a20
Matheus Felipe de Oliveira Ferreira <theusf40@hotmail.com>
2022-09-20 00:30:07 +0000

    Adicionado opção de escrever letra na tela

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,23 @@
+window.onload = () => desenharPalavra(palavraSecreta)
 const btn = document.getElementById('btn')
 const canvas = document.getElementById('jogo')
 btn.addEventListener("click", escolherPalavra)
+document.addEventListener('keypress', (event) => verificaPalavra(event.key))
 
 let palavras = ['papagaio', 'teste', 'banana', 'batata']
+let palavraSecreta = escolherPalavra()
+let xAxis = 25;
 
 function escolherPalavra() {
     let palavraSecreta = palavras[Math.floor(Math.random() * palavras.length)]
     console.log(palavraSecreta)
-    desenharPalavra(palavraSecreta)
+    return palavraSecreta
 }
 
 function desenharPalavra(palavra) {
     if (canvas.getContext) {
         const caneta = canvas.getContext('2d')
-        let x = 25;
+        let x = 25
 
         for(let i = 0; i < palavra.length; i++) {
             caneta.fillStyle = '#0A3871'
@@ -22,6 +26,18 @@ function desenharPalavra(palavra) {
             x += 35
         }
     }
+}
+
+function verificaPalavra(letra) {
+    if ((/[a-zA-Z]/).test(letra)) {
+        const caneta = canvas.getContext('2d')
+        caneta.font = '16px Arial'
+        caneta.fillText(letra, xAxis, 120)
+        xAxis += 25
+
+
+    }
+    else console.log('erro')
 }
 
 function irAdd() {
