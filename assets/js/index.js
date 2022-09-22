08c3ee467812e03297f889a30f039ae3ecec34a2
Matheus Felipe de Oliveira Ferreira <theusf40@hotmail.com>
2022-09-22 01:39:22 +0000

    Refeito criação dos traços

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,11 @@
 window.onload = () => desenharPalavra(palavraSecreta)
 const btn = document.getElementById('btn')
-const canvas = document.getElementById('jogo')
+let forca = document.getElementById('forca')
 btn.addEventListener("click", escolherPalavra)
 document.addEventListener('keypress', (event) => verificaPalavra(event.key))
 
-let palavras = ['papagaio', 'teste', 'banana', 'batata']
+let palavras = ['papagaio']
 let palavraSecreta = escolherPalavra()
-let xAxis = 25;
 
 function escolherPalavra() {
     let palavraSecreta = palavras[Math.floor(Math.random() * palavras.length)]
@@ -15,27 +14,18 @@ function escolherPalavra() {
 }
 
 function desenharPalavra(palavra) {
-    if (canvas.getContext) {
-        const caneta = canvas.getContext('2d')
-        let x = 25
+    let divPalavra = document.getElementById('holder')
 
-        for(let i = 0; i < palavra.length; i++) {
-            caneta.fillStyle = '#0A3871'
-            caneta.fillRect(x, 125, 25, 2)
-
-            x += 35
-        }
+    for(let i = 0; i < palavra.length; i++) {
+        let tag = document.createElement('div')
+        tag.classList.add('tracos')
+        divPalavra.appendChild(tag)
     }
 }
 
 function verificaPalavra(letra) {
     if ((/[a-zA-Z]/).test(letra)) {
-        const caneta = canvas.getContext('2d')
-        caneta.font = '16px Arial'
-        caneta.fillText(letra, xAxis, 120)
-        xAxis += 25
-
-
+        
     }
     else console.log('erro')
 }
